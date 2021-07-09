3bfd2faebd057c6f81af48d6de777e2ea28e8777
Jason Freeberg <jafreebe@microsoft.com>
2021-07-09 16:27:32 -0700

    Update settings.py

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -124,4 +124,20 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # If this is running on App Service, import the production config and overwrite the previous configs
 if 'WEBSITE_HOSTNAME' in os.environ:
-    from .production import *
+    ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+
+    # WhiteNoise configuration
+    MIDDLEWARE = [                                                                   
+        'django.middleware.security.SecurityMiddleware',
+    # Add whitenoise middleware after the security middleware                             
+        'whitenoise.middleware.WhiteNoiseMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',                      
+        'django.middleware.common.CommonMiddleware',                                 
+        'django.middleware.csrf.CsrfViewMiddleware',                                 
+        'django.contrib.auth.middleware.AuthenticationMiddleware',                   
+        'django.contrib.messages.middleware.MessageMiddleware',                      
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',                    
+    ]
+
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
