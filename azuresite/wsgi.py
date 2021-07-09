905c3ababed1ecd16d0a52fdab3fb5045b0ec029
Jason Freeberg <jafreebe@microsoft.com>
2021-07-09 16:36:11 -0700

    Update wsgi.py

--- azuresite/wsgi.py
+++ azuresite/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
 # on Azure App Service and should use the production settings in production.py.
 # settings_module = "azuresite.production" if 'WEBSITE_HOSTNAME' in os.environ else 'azuresite.settings'
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azuresite.settings')
 
 application = get_wsgi_application()
